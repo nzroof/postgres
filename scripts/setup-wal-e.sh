23bb5f2029003c8a8214d109f3a4b21f52caf713
Graeme Gellatly <gdgellatly@gmail.com>
2016-08-18 13:47:18 +1200

    [FIX] Replace else if with elif in wal-e setup script

--- scripts/setup-wal-e.sh
+++ scripts/setup-wal-e.sh
@@ -6,7 +6,7 @@ if [ "$CONTAINER_TYPE" = "S3" ]; then
     echo "$SECRET_KEY" > /etc/wal-e.d/env/AWS_SECRET_ACCESS_KEY
     echo "$ACCESS_KEY" > /etc/wal-e.d/env/AWS_ACCESS_KEY_ID
     echo "$BUCKET" > /etc/wal-e.d/env/WALE_S3_PREFIX
-else if [ "$CONTAINER_TYPE" = "AZURE" ]; then
+elif [ "$CONTAINER_TYPE" = "AZURE" ]; then
     echo "$SECRET_KEY" > /etc/wal-e.d/env/WABS_ACCESS_KEY
     echo "$ACCESS_KEY" > /etc/wal-e.d/env/WABS_ACCOUNT_NAME
     echo "$BUCKET" > /etc/wal-e.d/env/WALE_WABS_PREFIX
